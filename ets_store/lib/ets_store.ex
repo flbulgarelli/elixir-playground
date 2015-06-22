da52da31769b282cc2fb9d83a4854e44a29a9df8
bossiernesto <bossi.ernestog@gmail.com>
2015-06-21 22:03:15 -0300

    Improved ets wrapper version to include multiple tables

--- ets_store/lib/ets_store.ex
+++ ets_store/lib/ets_store.ex
@@ -1,30 +1,47 @@
+defmodule ListSearch do
+
+  def search_pattern([], element_atom) do
+    {:error, :none_found}
+  end
+  def search_pattern([x|xs], element_atom) do
+    case x == element_atom do
+      true -> {:ok, x}
+      false -> search_pattern(xs, element_atom)
+    end
+  end
+end
+
 defmodule EtsStore do
-    @table_id __MODULE__
 
-	def init do
-	  :ets.new(@table_id, [:public, :named_table])
-	end
+  def new_table(table_name) do
+    :ets.new(table_name, [:public, :named_table])
+  end
 
-	def insert(key, value) do
-	  :ets.insert(@table_id, {key, value})
-	end
+  def is_table_defined(table_name) do
+    ListSearch.search_pattern(:ets.all(),table_name)
+  end
 
-	def get(key) do
-	  case :ets.lookup(@table_id, key) do
+  def get(table, key) do
+    case :ets.lookup(table, key) do
 		  [{_key, value}] -> {:ok, value}
 		  [] -> {:error, :not_found}
 	  end
+  end
+
+  def put(table, key, value) do
+    :ets.insert(table, {key, value})
+  end
+
+  def delete(table, key) do
+	  :ets.match_delete(table, {key, :_})
 	end
 
-	def delete(key) do
-	  :ets.match_delete(@table_id, {key, :_})
-	end
-
-	def update(key, new_value) do
-	  case :ets.lookup(@table_id, key) do
-		[{_key, value}] -> delete(key)
-						   insert(key, new_value)
+	def update(table, key, new_value) do
+	  case :ets.lookup(table, key) do
+		[{_key, value}] -> delete(table, key)
+						   put(table, key, new_value)
 		[] -> {:error, :not_found}
 	  end
 	end
+
 end
--- ets_store/test/ets_store_test.exs
+++ ets_store/test/ets_store_test.exs
@@ -1,41 +1,70 @@
 import EtsStore
+import ListSearch
+
+defmodule ListSearchTest do
+  use ExUnit.Case
+
+  test "Search pattern on empty list" do
+    list = []
+    assert ListSearch.search_pattern(list, 2) == {:error, :none_found}
+  end
+
+  test "Search inexistent value on list" do
+    list = [1,2,4,8,16,32]
+    assert ListSearch.search_pattern(list, 31) == {:error, :none_found}
+  end
+
+  test "Search existent value on list" do
+    list = [1,2,4,8,16,32]
+    assert ListSearch.search_pattern(list, 4) == {:ok, 4}
+  end
+
+end
 
 defmodule EtsStoreTest do
   use ExUnit.Case
 
-  test "initialize, set 1 and get it" do
-    EtsStore.init
-    EtsStore.insert("one", 1)
+  test "add table and check if the tables are defined or not" do
+    EtsStore.new_table(:onetable)
+    assert EtsStore.is_table_defined(:onetable) == {:ok, :onetable}
+    assert EtsStore.is_table_defined(:some_table) == {:error, :none_found}
+  end
 
-    assert EtsStore.get("one") == {:ok, 1}
+  test "initialize, set 1 and get it" do
+    EtsStore.new_table(:some_table)
+    EtsStore.put(:some_table, "one", 1)
+
+    assert EtsStore.get(:some_table, "one") == {:ok, 1}
   end
 
   test "set multiple and get" do
-    EtsStore.init
-    EtsStore.insert("one", 1)
-    EtsStore.insert(:two, 2)
+    EtsStore.new_table(:some_table)
+    assert EtsStore.is_table_defined(:some_table) == {:ok, :some_table}
+    EtsStore.put(:some_table, "one", 1)
+    EtsStore.put(:some_table, :two, 2)
 
-    assert EtsStore.get("two") == {:error, :not_found}
-    assert EtsStore.get(:two) == {:ok, 2}
+    assert EtsStore.get(:some_table, "two") == {:error, :not_found}
+    assert EtsStore.get(:some_table, :two) == {:ok, 2}
   end
 
   test "set and delete" do
-    EtsStore.init
-    EtsStore.insert(:something, "something")
+    EtsStore.new_table(:some_table)
+    EtsStore.put(:some_table, :something, "something")
 
-    assert EtsStore.get(:something) == {:ok, "something"}
+    assert EtsStore.get(:some_table, :something) == {:ok, "something"}
 
-    assert EtsStore.delete(:something)
-    assert EtsStore.get(:something) == {:error, :not_found}
+    assert EtsStore.delete(:some_table, :something)
+    assert EtsStore.get(:some_table, :something) == {:error, :not_found}
   end
 
   test "set and update" do
-    EtsStore.init
-    EtsStore.insert(:one, 1)
+    EtsStore.new_table(:some_table)
+    EtsStore.put(:some_table, :one, 1)
 
-    assert EtsStore.get(:one) == {:ok, 1}
+    assert EtsStore.get(:some_table, :one) == {:ok, 1}
 
-    EtsStore.update(:one, "one")
-    assert EtsStore.get(:one) == {:ok, "one"}
+    EtsStore.update(:some_table, :one, "one")
+    assert EtsStore.get(:some_table, :one) == {:ok, "one"}
   end
+
 end
